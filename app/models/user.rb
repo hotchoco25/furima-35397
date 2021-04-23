711694b9afc16a11f5ae84824e7195c640df7c76
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-04-23 13:28:10 +0900

    Revert "キャンセル"
    This reverts commit 8bfa41cad189e7c7609fc861be298a166fdf2ca9.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,23 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  #PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  #NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-  #NAME_REGEX_MESSAGE = "Full-width characters"
-  #NAME_KANA_REGEX = /\A[ァ-ヶ一]+\z/
-  #NAME_KANA_REGEX_MASSAGE = "Full-width katakana characters"
-
-  #validates_format_of :password, with: PASSWORD_REGEX, message: "Include both letters and numbers"
-  #validates :nickname, presence: true
-  #validates :last_name, presence: true
-  #validates_format_of :last_name, with: NAME_REGEX, message: NAME_REGEX_MESSAGE
-  #validates :first_name, presence: true
-  #validates_format_of :first_name, with: NAME_REGEX, message: NAME_REGEX_MESSAGE
-  #validates :last_kana, presence: true
-  #validates_format_of :last_kana, with: NAME_KANA_REGEX, message: NAME_KANA_REGEX_MASSAGE
-  #validates :first_kana, presence: true
-  #validates_format_of :first_kana, with: NAME_KANA_REGEX, message: NAME_KANA_REGEX_MASSAGE
-  #validates :birth, presence: true
-
 end
