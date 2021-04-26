cde9e752a0e7ba77af0f40515f97453fe2dda0f6
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-04-26 14:39:39 +0900

    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,17 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :item_name, :text, :category_id, :status_id, :pay_id, :area_id, :day_id, :price, :image
+    ).merge(user_id: current_user.id)
   end
 end
