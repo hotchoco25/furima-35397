e1423830337e909ef0f5455497e4382509b4c681
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-04-26 19:25:08 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,23 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiverecordExtensions
-  belongs_to [:category, :status, :pay, :area, :day]
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :pay
+  belongs_to :area
+  belongs_to :day
+
+  with_options presence: true do
+    validates :item_name
+    validates :text
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :pay_id
+      validates :area_id
+      validates :day_id
+    end
+    validates :price
+  end
 end
