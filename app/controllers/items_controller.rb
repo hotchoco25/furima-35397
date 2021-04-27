d3bb2a0ba58a9b007dd2d2b9feb820266ed7951f
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-04-27 15:50:38 +0900

    実装条件に合わせた調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_session, except: :index
+
   def index
   end
 
@@ -8,12 +10,24 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
+
   def item_params
     params.require(:item).permit(
       :item_name, :text, :category_id, :status_id, :pay_id, :area_id, :day_id, :price, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,15 +10,17 @@ class Item < ApplicationRecord
   belongs_to :day
 
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :text
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 1, message: " Select" } do
       validates :category_id
       validates :status_id
       validates :pay_id
       validates :area_id
       validates :day_id
     end
-    validates :price
+    validates :price, numericality: { only_integer: true, message: "Half-width number" },
+                      inclusion: { in: 300..9999999, message: "Out of setting range" }
   end
 end
