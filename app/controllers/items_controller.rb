a82f419e56e4dda1e17e64bb1cb57a9915f2b755
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-04-27 19:34:13 +0900

    商品出品機能　修正2回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_session, except: :index
+  before_action :authenticate_user!, except: :index
 
   def index
   end
@@ -25,9 +25,4 @@ class ItemsController < ApplicationController
     ).merge(user_id: current_user.id)
   end
 
-  def move_to_session
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
-  end
 end
