42e786d78af84b234a17d5c35af175c7e87ba7cc
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-04-27 18:37:03 +0900

    コードの整形

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,12 +5,12 @@ FactoryBot.define do
     end
     nickname { Faker::Name.initials }
     email { Faker::Internet.free_email }
-    password { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
+    password { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     last_name { jp_name.last.kanji }
     first_name { jp_name.first.kanji }
     last_kana { jp_name.last.katakana }
     first_kana { jp_name.first.katakana }
-    birth { Faker::Date.birthday(min_age: 4,max_age: 90)}
+    birth { Faker::Date.birthday(min_age: 4, max_age: 90) }
   end
 end
