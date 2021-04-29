44337e7cb4d8cb38b19abed20735279be7b1d44d
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-04-29 19:14:01 +0900

    ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :redirect_path, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :redirect_path, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC").includes(:user)
@@ -32,6 +32,10 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
   end
 
   private
