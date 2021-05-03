339e65d61519729535cee476cfd6c34a4f44f609
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-05-03 11:10:28 +0900

    登録機能の動作の調整

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,7 @@
 class RecordsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @record_buy = RecordBuy.new
   end
 
   def create
@@ -9,6 +10,7 @@ class RecordsController < ApplicationController
       @record_buy.save
       redirect_to item_path(item.id)
     else
+      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -16,7 +18,7 @@ class RecordsController < ApplicationController
   private
 
   def record_params
-    params.require(:record_buy).premit(:post_code, :area_id, :muni, :add, :buil, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:record_buy).permit(:post_code, :area_id, :muni, :add, :buil, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
   end
-  
+
 end
--- app/models/record_buy.rb
+++ app/models/record_buy.rb
@@ -2,20 +2,21 @@ class RecordBuy
   include ActiveModel::Model
   attr_accessor :post_code, :area_id, :muni, :add, :buil, :tel, :user_id, :item_id
 
-  validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly'}
+  validates :post_code,presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
+  validates :area_id, numericality: { other_than: 1, message: " Select" }
+
   with_options presence: true do
     validates :user_id
     validates :item_id
 
-    validates :area_id, numericality: { other_than: 1, message: " Select" }
     validates :muni
     validates :add
-    validates :tel, format: {with: /\A[0-9]\z/, message: 'Input only number'}
+    validates :tel, format: { with: /\A[0-9]\z/, message: 'Input only number' }
   end
 
   def save
     record = Record.create(user_id: user_id, item_id: item_id)
 
-    Buy.save(post_code: post_code, area_id: area_id, muni: muni, add: add, buil: buil, tel: tel, record_id: record.id)
+    Buy.create(post_code: post_code, area_id: area_id, muni: muni, add: add, buil: buil, tel: tel, record_id: record.id)
   end
 end
