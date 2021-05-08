0845f363f01a981dca6c5e4f6b380469137c1a4f
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-05-08 22:41:10 +0900

    paramsとアソシエーションの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,13 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: :create
+
   def create
+    @comment = Comments.create(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: current_item.id)
   end
 end
