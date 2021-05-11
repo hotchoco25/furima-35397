5003e0314a04f170b6b62bc916bc64fe8b2e430f
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-05-11 11:00:24 +0900

    単体テストコードの実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメントの投稿' do
+    context 'コメントが投稿できるとき' do
+      it 'textがあれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントが投稿できないとき' do
+      it 'textが空では投稿できない' do
+        @comment.text = ""
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Text can't be blank")
+      end
+    end
+  end
 end
