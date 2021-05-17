5d04f43d4f51e18c5ce2e9d2b01a8fc3a97d7a52
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-05-17 13:14:36 +0900

    paramsの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -12,6 +13,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :nickname, :last_name, :first_name, :last_kana, :first_kana, :birth
+    ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [
       :nickname, :last_name, :first_name, :last_kana, :first_kana, :birth
     ])
   end
