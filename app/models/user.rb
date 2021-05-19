a00b56faa388870b61d48f01fb9283505290978e
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-05-19 12:04:39 +0900

    ユーザー情報の更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,29 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def update_without_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  PASSWORD_REGEX_MESSAGE = "Include both letters and numbers"
+  PASSWORD_REGEX_MESSAGE = "を半角英数字で入力してください"
   NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
-  NAME_REGEX_MESSAGE = "Full-width characters"
+  NAME_REGEX_MESSAGE = "を全角で入力してください"
   NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  NAME_KANA_REGEX_MESSAGE = "Full-width katakana characters"
+  NAME_KANA_REGEX_MESSAGE = "を全角カタカナで入力してください"
 
   with_options presence: true do
     validates :nickname
-    validates_format_of :password, with: PASSWORD_REGEX, message: PASSWORD_REGEX_MESSAGE
+    validates :password, format: { with: PASSWORD_REGEX, message: PASSWORD_REGEX_MESSAGE }, on: :create
     with_options format: { with: NAME_REGEX, message: NAME_REGEX_MESSAGE } do
       validates :last_name
       validates :first_name
