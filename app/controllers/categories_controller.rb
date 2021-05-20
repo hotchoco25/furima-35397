931298c27b52b22b4fc0a35f771feed56580cd68
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-05-20 14:14:10 +0900

    パスの設定とビューの調整

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,17 @@
 class CategoriesController < ApplicationController
+  before_action :redirect_path, only: :show
+
   def show
     @category = Category.find(params[:id])
     @items = @category.items.order("created_at DESC").includes(:user)
+  end
+
+  private
+
+  def redirect_path
+    category = Category.find(params[:id])
+    if category.id == 1
+      redirect_to root_path
+    end
   end
 end
