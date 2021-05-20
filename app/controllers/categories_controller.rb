4c4a85fb5020363d96d2bd303072bed464974efb
hotchoco25 <house.foods.books.and.game@gmail.com>
2021-05-20 13:06:27 +0900

    ビューの実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,6 @@
 class CategoriesController < ApplicationController
+  def show
+    @category = Category.find(params[:id])
+    @items = @category.items.order("created_at DESC").includes(:user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :items do
     resources :records, only: [:index, :create]
     resources :comments, only: [:create, :destroy]
-    resources :categories, only: :show
   end
   resources :users, only: :show
+  resources :categories, only: :show
 end
